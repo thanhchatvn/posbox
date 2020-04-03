e9a66cf025e0ecd00042f1e2af080643e71598a6
Bruce Nguyễn <trungthanh.nguyen@dnpcorp.vn>
2020-04-03 12:24:33 +0700

    update source

--- addons/hw_screen/__manifest__.py
+++ addons/hw_screen/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Screen Driver',
-    'version': '1.0.0.1',
+    'version': '1.0',
     'category': 'Hardware Drivers',
     'sequence': 6,
     'summary': 'Provides support for customer facing displays',
