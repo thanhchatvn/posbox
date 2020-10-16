1bcb84cb5d4c95655bd4ef058a4e7c3821f8f66d
Bruce Nguyễn <trungthanh.nguyen@dnpcorp.vn>
2020-10-17 00:04:20 +0700

    finish script for 17.posbus

--- rebuildPOSBOX17.sh
+++ rebuildPOSBOX17.sh
@@ -5,9 +5,9 @@
 sudo su
 mount -o remount,rw / && cd /home/pi/odoo/addons
 rm -rf hw_escpos && rm -rf hw_screen && cd ..
-rm -rf 17.zip* && rm -rf posbox-17*
+rm -rf 17.* && rm -rf posbox-*
 wget https://github.com/thanhchatvn/posbox/archive/17.posbus.zip
-unzip 17.zip && cd posbox-17
+unzip 17.posbus.zip && cd posbox-17.posbus
 cp -R hw_escpos /home/pi/odoo/addons && cp -R hw_screen /home/pi/odoo/addons
 cd /home/pi/odoo && chown pi.pi -R addons
 sudo reboot now
