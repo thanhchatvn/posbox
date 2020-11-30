b90d7556b499d12ff5080e4c7134ecf9ccdf9d77
Bruce Nguyen <thanhchatvn@gmail.com>
2020-11-30 11:42:07 +0700

    new branch posbox 20.10

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 sudo su
 echo "mount disk"
 mount -o remount,rw / && cd /home/pi/odoo/addons
-echo "remove 2 modules"
-rm -rf hw_escpos && rm -rf hw_drivers && cd ..
-rm -rf 20.06.* && rm -rf posbox-*
+echo "remove hw_escpos modules"
+rm -rf hw_escpos && cd ..
+rm -rf 20.10.* && rm -rf posbox-*
 echo "cloning source"
-wget https://github.com/thanhchatvn/posbox/archive/20.06.zip
-unzip 20.06.zip && cd posbox-20.06
+wget https://github.com/thanhchatvn/posbox/archive/20.10.zip
+unzip 20.10.zip && cd posbox-20.10
 echo "replace old source"
-cp -R hw_escpos /home/pi/odoo/addons && cp -R hw_drivers /home/pi/odoo/addons
+cp -R hw_escpos /home/pi/odoo/addons
 echo "grant permission new source"
 cd /home/pi/odoo && chown pi.pi -R addons && cd /home/pi/odoo && chown pi.pi -R addons
 echo "booting"
